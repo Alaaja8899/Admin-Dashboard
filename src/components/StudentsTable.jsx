126821eda897970fbf8dcaa0b024a98f393299be
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2025-01-01 18:12:16 +0300

    student update model added

--- src/components/StudentsTable.jsx
+++ src/components/StudentsTable.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Space, Table , Button, Modal, Popconfirm, message } from 'antd';
+import { Space, Table , Button, Modal, Popconfirm, message, Select } from 'antd';
 import { PiEyeClosed } from 'react-icons/pi';
 import { BiEdit, BiTrash } from 'react-icons/bi';
 import { Input} from 'antd';
@@ -11,6 +11,13 @@ const { Search } = Input;
 function StudentsTable() {
   const [dummyJson,setDummy]=useState([])
   const [modalOpen,setModalOpen] = useState(false)
+  const [Faculties,setFaculties]=useState([])
+  const [departments,setDepartments]=useState([])
+  
+  const [stdName,setStdName]=useState('')
+  const [id,setId]=useState()
+  const [Faculty,setFaculty]=useState('')
+  const [department,setDepartment]=useState('')
 
 
 
@@ -43,7 +50,13 @@ function StudentsTable() {
       key: 'action',
       render: (_, record) => (
         <Space size="middle" className='cursor-pointer'>
-          <EditOutlined/>
+          <EditOutlined onClick={()=>{
+            setStdName(record.name)
+            setDepartment(record.departmentid)
+            setId(record.id)
+            setFaculty(record.facultyid)
+            setModalOpen(true)
+          }}/>
 
             <Popconfirm okText='Yes' cancelText='No' title='Delete Student' description={`Are Sure to delete Student : ${record.name}`}
             onConfirm={()=>message.success("Successfully delted")}
@@ -69,12 +82,6 @@ function StudentsTable() {
   ];
 
 
-    const showModal=()=>{
-        setModalOpen(true)      
-    }
-    const hideModal=()=>{
-        setModalOpen(false)      
-    }
   
 
   const onSearch = (e) => {
@@ -84,6 +91,23 @@ function StudentsTable() {
 
 
   };
+
+      const handleOk = () => {
+        setModalOpen(false);
+  
+        if(!stdName || !id || !Faculty || !department){
+          message.error("All Fileds Not completed!")
+          return
+        }
+  
+        message.success("Student data recieved !")
+  
+        console.log("Student saving data : ",{name:stdName,id:id,faculty:Faculty,department:department})
+      };
+      const handleCancel = () => {
+        setModalOpen(false);
+      };
+  
 
   useEffect(() => {
     fetch('https://dummyjson.com/users')
@@ -105,6 +129,38 @@ function StudentsTable() {
     console.log("After filteration:",dummyJson)
   },[dummyJson])
 
+
+
+      useEffect(() => {
+        fetch('https://dummyjson.com/users')
+          .then(res => res.json())
+          .then(data => {
+            const transformedData = data.users.map(user => ({
+    
+              value: user.company?.address?.state || 'N/A',
+              label: user.company?.address?.state || 'N/A',
+            }));
+            setDepartments(transformedData)
+          })
+          .catch(err => console.error('Error fetching data:', err));
+      }, []);
+      useEffect(() => {
+        fetch('https://dummyjson.com/users')
+          .then(res => res.json())
+          .then(data => {
+            const transformedData = data.users.map(user => ({
+              value: user.company?.department || 'N/A',
+              label: user.company?.department || 'N/A',
+              key:Math.random(1,100)
+    
+            }));
+            setFaculties(transformedData)
+          })
+          .catch(err => console.error('Error fetching data:', err));
+      }, []);
+    
+
+
   return (
     <div className='p-3'>
        <Search
@@ -116,15 +172,26 @@ function StudentsTable() {
     />
 
 
-      <Modal
-          open={modalOpen}
-          okText='Yes'
-          cancelText='No'
-          title="Update"    
-      >
+      <Modal 
+            className='flex flex-col gap-4'
+            title="UpdateStudent" open={modalOpen} onOk={handleOk} onCancel={handleCancel} okText="Save">
+              <Input placeholder='Student Name' onChange={(e)=> setStdName(e.target.value)} value={stdName} />
+              <br />
+              <Input placeholder='ID' maxLength={6}  onChange={(e)=> setId(e.target.value)} value={id} />
+              <br />
+              <Select className='w-full' onChange={(e)=> setFaculty(e)}
+              defaultValue={Faculty ? Faculty:"Select Faculty"}
+              options={Faculties}
+              >
 
+              </Select>
+              <br />
+              <Select className='w-full' onChange={(e)=> setDepartment(e)}
+              defaultValue={department ? department :"Select Department"}
+              options={departments}
+              >
 
-
+        </Select>
       </Modal>
 
 
